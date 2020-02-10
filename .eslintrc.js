19ec24da150bba23d88c39e563c0e94d83aa134d
Ruslan Tupolev <36785258+rtplv@users.noreply.github.com>
2020-02-10 13:30:35 +0300

    Update eslint rules for semi-columns
    Two styleguide conflicted

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,10 @@ module.exports = {
     '@vue/typescript'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    semi: [ 2, 'always' ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    "semi": "off",
+    "@typescript-eslint/semi": ["error"],
     "comma-dangle": 0,
     'array-bracket-spacing': "off",
     "vue/no-v-html": 0,
